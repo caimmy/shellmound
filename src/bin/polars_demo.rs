81e11e0acf2e9b73d9b92be439115df152d8adda
caimm <caimmy@qq.com>
2024-03-10 14:37:24 +0800

    t

---
+++ src/bin/polars_demo.rs
@@ -0,0 +1,19 @@
+use polars::prelude::*;
+use chrono::NaiveDate;
+
+fn main() {
+    
+    let mut df = df!(
+        "integer" => &[1, 2, 3],
+        "date" => &[
+                NaiveDate::from_ymd_opt(2025, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        ],
+        "float" => &[4.0, 5.0, 6.0],
+        "string" => &["a", "b", "c"],
+    )
+    .unwrap();
+    let r = df.select([col("integer"), col("float")]).collect();
+    println!("{}", r);
+}
